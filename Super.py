083399d2fdb8d6c3e8bd415eef4d2da3401df8c7
MR DARK <105338726+JISAN-404@users.noreply.github.com>
2022-09-26 18:40:51 +0600

    Create Super.py

---
+++ Super.py
@@ -0,0 +1,26 @@
+import os, platform
+ 
+try:
+ 
+        import requests
+ 
+except:
+ 
+        os.system('pip2 install requests')
+ 
+ 
+ 
+import requests
+ 
+bit = platform.architecture()[0]
+ 
+if bit == "64bit":
+ 
+        import Love64
+ 
+ 
+ 
+elif bit == "32bit":
+ 
+        import Love32
+ 
